00e9e9bf580ca44692d9d5ce35dd535333253c25
sgc93 <smachewgedefaw@gmail.com>
2024-04-04 06:55:25 -0900

    display requirements with sample simulated id

--- src/features/requirement/IDReq.jsx
+++ src/features/requirement/IDReq.jsx
@@ -1,58 +1,64 @@
-import { useState } from "react";
-import { IoAlert, IoClose, IoIdCard } from "react-icons/io5";
+import { IoAlert } from "react-icons/io5";
 
 const rules = [
 	"The file name of your picture should be your id number",
 	"Do not attempt to re-apply",
 	"You should use jpg, png and jpeg image formats",
-	"blurred image or image with unseen ears will not be acceptable",
+	"Blurred image or image with unseen ears will not be acceptable",
 ];
 
-export default function IDReq() {
-	const [isOpen, setIsOpen] = useState(false);
-
-	return isOpen ? (
-		<ReqBox />
-	) : (
-		<div className="flex items-center gap-3 mr-[-5rem]">
-			<span className="text-xl text-stone-400 transition-all duration-300 hover:bg-stone-400 hover:text-stone-800 px-2">
-				<IoIdCard />
-			</span>
-			<span className="glassmorphism-white px-2 p-[2px] rounded-full">
-				rules
-			</span>
+export default function IDReq({ simulate }) {
+	return (
+		<div className="w-screen h-screen flex flex-col md:flex-row items-center justify-center">
+			<ReqBox simulate={simulate} />
+			<div className="h-full w-1/2 flex items-center justify-center bg p-10">
+				<div className="glassmorphism-white p-4 rounded-xl">
+					<img
+						src="Name_FName_GFName_.jpg"
+						alt="smple_id"
+						className="rounded-xl"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
 
-function ReqBox() {
+function ReqBox({ simulate }) {
 	return (
-		<div className="glassmorphism-white w-full p-2 rounded-xl">
+		<div className="reverse_bg flex flex-col justify-between border-2 border-[var(--color-slight-white)] w-1/2 h-1/2 p-2 rounded-xl m-10">
 			<div className="flex flex-col gap-2">
 				<div className="flex flex-col">
 					<div className="pb-1 flex items-center justify-between">
-						<span className="text-lg font-bold text-blue-950">
+						<span className="text-lg font-bold text-slate-300">
 							Application Rules
 						</span>
-						<IoClose className="text-xl rounded-full transition-all duration-300 hover:bg-stone-800 hover:text-stone-100" />
 					</div>
-					<span className="w-full h-[1px] bg-stone-700"></span>
+					<span className="w-full h-[1px] bg-[var(--color-slight-white)]"></span>
 				</div>
-				<div className="flex flex-col gap-2">
+				<div className="flex flex-col items-center pt-10 gap-2">
 					<span className="glassmorphism-bg px-2 py-[2px] rounded-lg font-semibold text-red-600">
 						Take caution for the following points while applying for digital id
 					</span>
-					<div className="flex flex-col  gap-1">
+					<div className="flex flex-col  gap-2">
 						{rules.map((rule, key) => (
 							<div key={key} className="flex items-center gap-2">
-								<IoAlert className="text-xl p-[3px] glassmorphism-bg rounded-full" />
-								<span className="text-stone-900 glassmorphism-bg rounded-full px-2">
+								<IoAlert className="text-2xl text-slate-400 p-[3px] rounded-full" />
+								<span className="text-slate-300 text-xl bg rounded-xl py-1 px-2">
 									{rule}
 								</span>
 							</div>
 						))}
 					</div>
 				</div>
+			</div>
+			<div className="w-full h-10 flex items-center justify-end">
+				<button
+					className="glassmorphism-white px-2 py-[2px]  rounded-lg text-lg font-semibold transition-all duration-300 hover:bg-slate-600 hover:text-slate-50"
+					onClick={() => simulate(true)}
+				>
+					start simulating
+				</button>
 			</div>
 		</div>
 	);
