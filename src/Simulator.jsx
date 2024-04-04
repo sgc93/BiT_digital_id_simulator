7bdbb18ef18fd7b06c7e33f9aa84f47ff482887b
sgc93 <smachewgedefaw@gmail.com>
2024-04-04 06:54:19 -0900

    handle toggleing between id simulator and requritement displayer components

--- src/Simulator.jsx
+++ src/Simulator.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import StudentId from "./features/id_card/StudentId";
+import IDReq from "./features/requirement/IDReq";
 
 export default function Simulator() {
-	const [isBoxOpened, setIsIdBoxOpened] = useState(true);
+	const [isBoxOpened, setIsIdBoxOpened] = useState(false);
+	function simulate(shouldSimulate) {
+		setIsIdBoxOpened(shouldSimulate);
+	}
 	return (
 		<div className="bg w-screen h-screen">
-			<StudentId setIsIdBoxOpened={setIsIdBoxOpened} />
+			{isBoxOpened && (
+				<div className="absolute left-0 top-0 z-30 w-screen h-screen backdrop-blur-[3px] flex items-center justify-center">
+					<StudentId simulate={simulate} />
+				</div>
+			)}
+			<IDReq simulate={simulate} />
 		</div>
 	);
 }
